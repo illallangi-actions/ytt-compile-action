e01470e57b6e0ade5c4b876212e5e4f0d38da83e
Andrew Cole <andrew.cole@illallangi.com>
2019-12-30 23:43:59 +1100

    Excluded dot directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ source=${SOURCE:-source}/
 target=${TARGET:-target}/
 temp=${TEMP:-$(mktemp -d -t ytt-compile-action-XXXXXXXXXX)}/
 
-echo rsync -zarv --include="*/" --include="*.yaml" --exclude="*" "${GITHUB_WORKSPACE}/${source}" "${temp}"
-rsync -zarv --include="*/" --include="*.yaml" --exclude="*" "${GITHUB_WORKSPACE}/${source}" "${temp}"
+echo rsync -zarv --exclude ".*/" --include="*/" --include="*.yaml" --exclude="*" "${GITHUB_WORKSPACE}/${source}" "${temp}"
+rsync -zarv --exclude ".*/" --include="*/" --include="*.yaml" --exclude="*" "${GITHUB_WORKSPACE}/${source}" "${temp}"
 
 echo ytt --debug --ignore-unknown-comments --strict --file "${temp}" --output yaml --output-directory "${GITHUB_WORKSPACE}/${target}"
 ytt --debug --ignore-unknown-comments --strict --file "${temp}" --output yaml --output-directory "${GITHUB_WORKSPACE}/${target}"
